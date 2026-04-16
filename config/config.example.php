30c95b88491125a2fc4af688bb0668ac074ec328
wuodo <willysonyango05@gmail.com>
2026-04-16 17:51:50 +0300

    Clean repo for GitHub deployment

--- config/config.example.php
+++ config/config.example.php
@@ -1,15 +1,14 @@
 <?php
 return [
     'app_name' => 'St. Mary\'s Mother and Child Hospital Medical Training College',
-    'base_url' => '',
+    'base_url' => 'https://your-domain.example',
     'db' => [
         'host' => 'localhost',
         'port' => '3306',
-        'database' => 'stmarys2_stm',
-        'username' => 'stmarys2_stm',
-        'password' => '@MbatiMariam2802',
+        'database' => 'your_database_name',
+        'username' => 'your_database_user',
+        'password' => 'your_database_password',
         'charset' => 'utf8mb4',
-	base_url => https://https://www.stmarysmchmcollege.ac.ke/
     ],
     'session_name' => 'stm_session',
     'admin_email' => 'admin@stm.ac.ke',
